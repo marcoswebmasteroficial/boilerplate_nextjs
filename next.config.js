11d6edc4d0df3ec063cc38cfbb288cc23fc16a50
marcoswebmasteroficial <48739175+marcoswebmasteroficial@users.noreply.github.com>
2022-10-18 16:07:27 -0300

    Update dependencias

--- next.config.js
+++ next.config.js
@@ -1,12 +1,29 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withPWA = require('next-pwa')
-const isProd = process.env.NODE_ENV === 'production'
+module.exports = withPWA()
 
-module.exports = withPWA({
+const settings = {
+  swcMinify: true,
+  compiler: {
+    styledComponents: {
+      ssr: true,
+      displayName: true,
+      fileName: true
+    }
+  },
   pwa: {
     dest: 'public',
-    disable: !isProd
-  },future: {
-    webpack5: true
+    fallbacks: {
+      image: '/static/images/fallback.png'
+    }
+  },
+  images: {
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    dangerouslyAllowSVG: false,
+    domains: ['localhost']
   }
-})
+}
+
+module.exports =
+  process.env.NODE_ENV === 'development' ? settings : withPWA(settings)

06830825ea9bb9ae0fd5f069f268c1a1a34c457d
marcoswebmasteroficial <48739175+marcoswebmasteroficial@users.noreply.github.com>
2022-10-19 08:39:15 -0300

    update pwa

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const withPWA = require('next-pwa')
 module.exports = withPWA()
 
+const Pwa = {
+  pwa: {
+    dest: 'public',
+    fallbacks: {
+      image: '/static/images/fallback.png'
+    }
+  }
+}
+
 const settings = {
   swcMinify: true,
   compiler: {
@@ -10,20 +19,8 @@ const settings = {
       displayName: true,
       fileName: true
     }
-  },
-  pwa: {
-    dest: 'public',
-    fallbacks: {
-      image: '/static/images/fallback.png'
-    }
-  },
-  images: {
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    dangerouslyAllowSVG: false,
-    domains: ['localhost']
   }
 }
 
 module.exports =
-  process.env.NODE_ENV === 'development' ? settings : withPWA(settings)
+  process.env.NODE_ENV === 'development' ? settings : withPWA(settings, ...Pwa)
